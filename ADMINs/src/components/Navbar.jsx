ac745744268ed907ff52c7fbeb13d5d6485637c0
prafful sharma <sharmaprafful646@gmail.com>
2025-08-25 21:32:23 +0530

    Update Navbar.jsx

--- ADMINs/src/components/Navbar.jsx
+++ ADMINs/src/components/Navbar.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import { assets } from "../admin_assets/assets";
-// const Navbar = ({ setToken }) => {
-//   return (
-//     <div className="flex items-center py-2 px-[4%] justify-between">
-//       <img className="w-[max(8%,80px)]" src={assets.prafful} alt="" />
-//       <button
-//         onClick={() => setToken("")}
-//         className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
-//       >
-//         Logout
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
 
 import React from "react";
 import { assets } from "../admin_assets/assets";
