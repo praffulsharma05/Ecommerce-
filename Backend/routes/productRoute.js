5585fc0492b97608579c3ff18cbae6e5cf8b1033
prafful sharma <sharmaprafful646@gmail.com>
2025-08-25 21:43:34 +0530

    Update productRoute.js

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,4 +1,4 @@
-import express from 'express';
+ import express from 'express';
 import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
 import adminAuth from '../middleware/adminAuth.js';
@@ -12,5 +12,7 @@ productRouter.post('/single',singleProduct);
 productRouter.get('/list',listProduct);
 
 export default productRouter;
+
+
 
 
